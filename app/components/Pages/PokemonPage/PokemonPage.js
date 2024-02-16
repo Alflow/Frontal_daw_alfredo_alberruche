639b881b8eed8bf622ecbea5537cca99fcaa445f
Alfredo Alberruche <aalberrh@gmail.com>
2024-02-16 19:49:27 +0100

    .

--- app/components/Pages/PokemonPage/PokemonPage.js
+++ app/components/Pages/PokemonPage/PokemonPage.js
@@ -6,10 +6,8 @@ export default function PokemonPage() {
   const $pokemonPage = document.createElement("div");
 
   const $headerPage = document.createElement("div");
-  $headerPage.classList = "col-12 text-center mb-4"
-  $headerPage.innerHTML = '<h1>¡Bienvenidos a la colección de España!</h1>'
-
-  
+  $headerPage.classList = "col-12 text-center mb-4";
+  $headerPage.innerHTML = "<h1>¡Bienvenidos a la colección de España!</h1>";
 
   $pokemonPage.appendChild($headerPage);
 
@@ -22,6 +20,25 @@ export default function PokemonPage() {
   $gallery.classList = "row col-12 row-cols-1 row-cols-md-4 g-5";
   $pokemonPage.appendChild($gallery);
 
+  const $pagination = document.createElement("nav");
+  $pagination.setAttribute("aria-label", "Page navigation example");
+  $pagination.innerHTML = `
+
+  // PENDIENTE USAR LA LÓGICA DE PAGINACIÓN. YA SE DEVUELVE TOTAL EN LA PETICIÓN 
+  
+  // Elemento de paginación.
+
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+
+  `;
+
+  // $gallery.appendChild($pagination);
   peticion({
     url: config_db.POKEMON,
     cbSuccess: (response) => {
