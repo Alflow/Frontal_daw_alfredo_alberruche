8b82b5511d5e6b093689a51ccab35ab408e4531c
Alfredo Alberruche <aalberrh@gmail.com>
2024-02-28 17:24:09 +0100

    Comprobación de integridad de datos en los inputs.

--- app/components/Pages/Forms/FormSignUp.js
+++ app/components/Pages/Forms/FormSignUp.js
@@ -2,16 +2,14 @@ import config_db from "../../../helpers/config_db.js";
 import { envio, envioRegistro } from "../../../helpers/peticion.js";
 import Router from "../../Router.js";
 export default function FormSignUp() {
-
   const $container = document.createElement("div");
-  
-  $container.classList = "d-flex flex-column align-items-start min-vh-100";
 
+  $container.classList = "d-flex flex-column align-items-start min-vh-100";
 
   const $form = document.createElement("form");
 
   $form.setAttribute("id", "form-signup");
-  $form.classList="col-md-5 mt-4 mx-auto"
+  $form.classList = "col-md-5 mt-4 mx-auto";
   $form.innerHTML = `
 
 <div class="mb-3">
@@ -29,33 +27,59 @@ export default function FormSignUp() {
   <label for="password" class="form-label">CONTRASEÑA</label>
   <input type="password" class="form-control" name="password" id="password">
 </div>
+<div class="mb-3">
+  <label for="password2" class="form-label">REPETIR CONTRASEÑA</label>
+  <input type="password" class="form-control" name="password2" id="password2">
+</div>
 
 
 <button type="submit" class="btn btn-primary">Enviar</button>
 
 `;
   $form.addEventListener("submit", (e) => {
-    // console.log(e);
+    // console.log("ESTÁN LOS CAMPOS RELLENOS? ->" + todosLosCamposRellenos());
+
     e.preventDefault();
     let form = new FormData($form);
 
-    let datos = {};
-    // Convierte FormData a un objeto JavaScript
-    form.forEach((value, key) => {
-      datos[key] = value;
-    });
+    // let datosIntroducidos = form.split("");
+    let arrayCampos = [];
+    arrayCampos.push(document.getElementById("email").value);
+    arrayCampos.push(document.getElementById("name").value);
+    arrayCampos.push(document.getElementById("password").value);
+    arrayCampos.push(document.getElementById("password2").value);
 
-    envioRegistro({
-      url: config_db.SIGNUP,
-      method: 'POST',
-      datos: JSON.stringify(datos),
-      cbSuccess: () => {  
-        alert("Usuario registrado con éxito!");
-        window.location.hash = "#home";      
-      },
-    });
+    console.log(arrayCampos);
+    let faltanCampos = false;
+    for (let i = 0; i < arrayCampos.length; i++) {
+      if (arrayCampos[i] == "") {
+        faltanCampos = true;
+        break;
+      }
+    }
+
+    if (!faltanCampos && arrayCampos[2] == arrayCampos[3]) {
+      let datos = {};
+      // Convierte FormData a un objeto JavaScript
+      form.forEach((value, key) => {
+        datos[key] = value;
+      });
+      // delete form.password2;
+      console.log(form);
+      envioRegistro({
+        url: config_db.SIGNUP,
+        method: "POST",
+        datos: JSON.stringify(datos),
+        cbSuccess: () => {
+          alert("Usuario registrado con éxito!");
+          window.location.hash = "#home";
+        },
+      });
+    } else {
+      alert("NO PUEDEN NI FALTAR CAMPOS NI TENER CONTRASEÑAS DIFERENTES");
+    }
   });
 
-  $container.appendChild($form)
+  $container.appendChild($form);
   return $container;
 }
